b0c1317f4b549827d3720f5c8b2bd220b4854a36
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2022-02-08 20:05:14 -0900

    add: `mapTable`, `concatMax`, `collatz`, & `backSubs` (passed checks)

--- proj2/pa2.lua
+++ proj2/pa2.lua
@@ -4,12 +4,62 @@
 -- 2022-02-02
 -- Exercise 2: Programming in Lua
 
-io.write("Hello, Lua module!\n")
+local pa2 = {} -- Module table
 
--- mapTable
+-- mapTable [EXPORTED]
+function pa2.mapTable(f, t)
+  local placeholder = {}
+  
+  for k, v in pairs(t) do
+    placeholder[k] = f(v)
+  end
+  
+  return placeholder
+end
 
--- concatMax
+-- concatMax [EXPORTED]
+function pa2.concatMax(input, iteration)
+  local concatenation, count, length = "", 0, string.len(input)
+  
+  for count = length, iteration, length do
+    concatenation = concatenation..input
+  end
+  
+  return concatenation
+end
 
--- collatz
+-- collatz [EXPORTED]
+function pa2.collatz(k)
+  local function sequence()
+    if k > 1 then
+      local save_collaz = k
+      if k % 2 == 0 then
+        k = k / 2
+      else
+        k = 3 * k + 1
+      end
+      return save_collaz
+    elseif k == 1 then
+      k = 0
+      return 1
+    else
+      return nil
+    end
+  end
+  return sequence, nil, nil
+end
 
--- backSubs
+-- backSubs [EXPORTED]
+function pa2.backSubs(s)
+  coroutine.yield("")
+  
+  local reversed, count, length = string.reverse(s), 1, string.len(s)
+  
+  for i = count, length do
+    for j = count, length - i + count do
+      coroutine.yield(string.sub(reversed, j, j + i - count))
+    end
+  end
+end
+
+return pa2 -- Return the module, so client code can use it.
