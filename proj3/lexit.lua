4b798cab3430498a7b9a98e000ce562b4dfb9a5e
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2022-02-08 22:20:35 -0900

    add: proj3 (initial files)

---
+++ proj3/lexit.lua
@@ -0,0 +1,11 @@
+#!/usr/bin/env lua
+-- lexit.lua
+-- Solomon Himelbloom
+-- 2022-02-08
+-- Exercise 2: Lexer in Lua
+
+local lexit = {} -- Module table
+
+io.write("Hello, Lexit!\n")
+
+return lexit -- Return the module, so client code can use it.
